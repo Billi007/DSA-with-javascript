94d224c208cbef69d89b496b56436ffc6b07f12f
= <=>
2025-04-03 15:44:15 +0530

    adding pattern sets

---
+++ Arrays/index.js
@@ -0,0 +1,21 @@
+
+
+let arr = [20,30,40,50,60]
+
+//While loop
+let result = [];
+let i= arr.length -1;
+
+while (i >= 0) {
+result.push(arr[i]);
+i--;
+}
+console.log(result)
+
+
+//For loop
+let result2 = [];
+for(let i = arr.length-1; i >= 0; i--) {
+    result2.push(arr[i]);
+}
+console.log(result2)                                                                                                                                                                                                                                                                                                                                                                                                                                 
